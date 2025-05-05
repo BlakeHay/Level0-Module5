3a28c3296b83cf815c0ec4f7ddc68ddc2e0247cd
BlakeHay <blake.christian.hay@gmail.com>
2025-05-05 23:58:35 +0000

    Monday May 5 2025

--- src/_03_method_writing/_1_obedient_robot/_1_obedient_robot.java
+++ src/_03_method_writing/_1_obedient_robot/_1_obedient_robot.java
@@ -1,14 +1,55 @@
 package _03_method_writing._1_obedient_robot;
+import javax.swing.JOptionPane;
+import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 public class _1_obedient_robot {
+static Robot rob = new Robot();
 public static void main(String[] args) {
-Robot	rob = new Robot();
+setup();
+String shape = JOptionPane.showOptionDialog("What shape would you like me to draw?", ("triangle", "square", "circle"));
+String color = JOptionPane.showInputDialog("What color would you like me to use?(red, blue, or yellow)");
+if(color.equalsIgnoreCase("red")){
+rob.setPenColor(Color.RED);
+}
+if(color.equalsIgnoreCase("blue")){
+rob.setPenColor(Color.BLUE);
+}
+if(color.equalsIgnoreCase("yellow")){
+rob.setPenColor(Color.YELLOW);
+}
+if(shape.equalsIgnoreCase("square")){
+drawSquare();
+}
+if(shape.equalsIgnoreCase("circle")){
+drawCircle();
+}
+if(shape.equalsIgnoreCase("triangle")){
+drawTriangle();
+}
+}
+public static void setup() {  
 rob.setX(300);
 rob.setY(300);
 rob.penDown();
-rob.setSpeed(5);
+rob.setSpeed(10);
+rob.miniaturize();
 }
-public static void draw() {
-    System.out.println("Method1");
+public static void drawSquare() {
+for(int i = 0; i < 4; i++){
+rob.move(100);
+rob.turn(90);
+}
+}
+public static void drawTriangle() {
+for(int i = 0; i < 3; i++){
+rob.move(100);
+rob.turn(120);
+}
+}
+public static void drawCircle() {
+for(int i = 0; i < 360; i++){
+rob.move(10);
+rob.turn(1);
+}
 }
 }
